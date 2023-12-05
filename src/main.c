dba7e9967947b2a0f3a5991119fcee765c7b38b4
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-05 22:29:43 +0100

    implmented basic function

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@ int main() {
     // Your test code for the allocator
     printf("Hello, World!\n");
 
+    my_malloc(10);
+
     return 0;
 }
 
--- src/my_allocator.c
+++ src/my_allocator.c
@@ -1,5 +1,15 @@
 #include "my_allocator.h"
+#include <stdio.h>
 // Include other necessary headers
 
 // Implement the functions declared in my_allocator.h
+void *my_malloc(size_t size) {
+    printf("%zu\n", size);
 
+    return NULL;
+}
+
+void my_free(void *ptr) {}
+
+void *my_realloc(void *ptr, size_t size) { return NULL; }
+void *my_calloc(size_t nmemb, size_t size) { return NULL; }
